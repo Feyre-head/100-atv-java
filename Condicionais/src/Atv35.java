6602a9efeac4b52762205275b63992cfdf164328
Feyre <inaraoenning@gmail.com>
2025-09-20 21:36:31 -0300

    Add Atv35

---
+++ Condicionais/src/Atv35.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Atv35 {
+    public static void main(String[] args) {
+        //ler3 num
+
+        Scanner sc = new Scanner(System.in);
+
+        int tamanho = 3;
+        int[] numeros = new int[tamanho];
+        for (int c = 0; c < 3; c++) {
+            System.out.println("Informe o " + (1 + c) + " número:");
+            numeros[c] = sc.nextInt();
+
+        }
+
+        // 1
+
+        if (numeros[0] <= numeros[1] && numeros[0] <= numeros[2]) {
+            System.out.print(numeros[0] + " ");
+            if (numeros[1] <= numeros[2]) {
+                System.out.print(numeros[1] + " " + numeros[2]);
+            } else {
+                System.out.print(numeros[2] + " " + numeros[1]);
+            }
+
+        } else if (numeros[1] <= numeros[0] && numeros[1] <= numeros[2]) {
+            System.out.print(numeros[1] + " ");
+            if (numeros[0] <= numeros[2]) {
+                System.out.print(numeros[0] + " " + numeros[2]);
+            } else {
+                System.out.print(numeros[2] + " " + numeros[0]);
+
+            }
+        } else {
+            System.out.print(numeros[2]);
+            if (numeros[0] <= numeros[1]) {
+                System.out.print(numeros[0] + " " + numeros[1]);
+            } else {
+                System.out.print(numeros[1] + " " + numeros[0]);
+            }
+
+        }
+
+        sc.close();
+    }
+
+}
+
+
